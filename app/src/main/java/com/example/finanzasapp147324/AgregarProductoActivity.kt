99ccb8218fdab55226e8510434090f4472eb455d
Sergio <sergio-armando-lopez@hotmail.com>
2021-12-07 20:49:09 -0600

    fix boton

--- app/src/main/java/com/example/finanzasapp147324/AgregarProductoActivity.kt
+++ app/src/main/java/com/example/finanzasapp147324/AgregarProductoActivity.kt
@@ -1,18 +1,45 @@
 package com.example.finanzasapp147324
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.IgnoreExtraProperties
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_agregar_producto.*
 
 class AgregarProductoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_producto)
+        val db = Firebase.firestore
 
 
         btn_guardarProducto.setOnClickListener {
+            val db = Firebase.firestore
+
+            // Create a new user with a first and last name
+            val user = hashMapOf(
+                "first" to "Ada",
+                "last" to "Lovelace",
+                "born" to 1815
+            )
+
+            // Add a new document with a generated ID
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(ContentValues.TAG, "Error adding document", e)
+                }
+
+
+            // Create a new user with a first and last name
+
 
         }
 
--- app/src/main/java/com/example/finanzasapp147324/RegistrarseActivity.kt
+++ app/src/main/java/com/example/finanzasapp147324/RegistrarseActivity.kt
@@ -1,11 +1,8 @@
 package com.example.finanzasapp147324
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_login.*
-
 
 
 import android.widget.EditText
@@ -28,7 +25,7 @@ class RegistrarseActivity : AppCompatActivity() {
             valida_registro()
             finish()
         }
-        btn_cancelarRegisro.setOnClickListener {
+        btn_cancelarRegistro.setOnClickListener {
             finish()
         }
     }
