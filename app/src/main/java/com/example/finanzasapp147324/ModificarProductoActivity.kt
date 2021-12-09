f1139838178bd81e97997bdbfecdde13c900b996
sergio <sergiolopesp@protonmail.com>
2021-12-08 23:05:07 -0700

    Actiity agregar producto

--- app/src/main/java/com/example/finanzasapp147324/ModificarProductoActivity.kt
+++ app/src/main/java/com/example/finanzasapp147324/ModificarProductoActivity.kt
@@ -18,6 +18,56 @@ class ModificarProductoActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_producto)
 
+        val db = Firebase.firestore
+
+      //  tv_agregarProductoNombre.setText("Hola")
+
+
+
+        tv_agregarProductoNombre.setText(getIntent().getSerializableExtra("id").toString())
+        tv_agregarProductoDescripcion.setText(getIntent().getSerializableExtra("id").toString())
+        tv_agregarProductoGastoMen.setText(getIntent().getSerializableExtra("id").toString())
+
+
+        val idProduct = getIntent().getSerializableExtra("id").toString()
+        try {
+
+
+
+                    val productoAuxMod = db.collection("productos").document(idProduct)
+
+                    productoAuxMod
+                        .update("id",idProduct)
+                        .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully updated!") }
+                        .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error updating document", e) }
+
+
+
+                .addOnFailureListener { e ->
+                    Log.w(ContentValues.TAG, "Error adding document", e)
+                }
+
+
+            // Create a new user with a first and last name
+            finish();
+        }
+        catch (e:Exception){
+            Toast.makeText(this,e.message, Toast.LENGTH_SHORT).show();
 
         }
+
+
+
+
+
+
+
+
+    btn_cancelaragregarproducto.setOnClickListener {
+            finish();
+            //val intent = Intent(this, MainActivity::class.java)
+            // start your next activity
+            //startActivity(intent)
+        }
+    }
     }
